d435d1e04673616c37fa590cba8a21339253badd
dusansykora <freewing.prod@gmail.com>
2016-01-22 21:35:23 +0100

    user edit + avatar

--- project/app/controllers/users/registrations_controller.rb
+++ project/app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
+before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
-  #   super    
+  #   super
   # end
 
   # POST /resource
@@ -49,9 +49,9 @@ before_filter :configure_sign_up_params, only: [:create]
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :dob, :password, :password_confirmation, :current_password, :avatar) }
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
