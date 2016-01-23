320bf666f7ba678996676c613181d6bffcacb3c5
dusansykora <freewing.prod@gmail.com>
2016-01-23 17:49:05 +0100

    fix of band model

--- project/app/models/band.rb
+++ project/app/models/band.rb
@@ -3,5 +3,5 @@ class Band < ActiveRecord::Base
   has_attached_file :cover_photo,
     :styles => { :medium => "300x300>", :thumb => "100x100#" },
     :default_url => "/images/:style/missing_band.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :cover_photo, :content_type => /\Aimage\/.*\Z/
 end
