c8ffc10978e817d71e79bc6406997f5ba7b5e389
Marek <NeveS07@azet.sk>
2016-01-24 18:02:49 +0100

    small changes + opinions index + added links to views

--- project/app/controllers/opinions_controller.rb
+++ project/app/controllers/opinions_controller.rb
@@ -1,4 +1,8 @@
 class OpinionsController < ApplicationController
+  def index
+    @band = Band.find(params[:band_id])
+  end
+  
   def new
     @band = Band.find(params[:band_id])
     @opinion = Opinion.new
