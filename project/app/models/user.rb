ed5272c158f24f2ba4ee89f8e278922200f30275
Marek <NeveS07@azet.sk>
2016-01-24 17:28:49 +0100

    opinions on band

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -11,5 +11,7 @@ class User < ActiveRecord::Base
     :styles => { :medium => "300x300>", :thumb => "100x100#" },
     :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
   belongs_to :band
+  has_many :opinions
 end
--- project/db/schema.rb
+++ project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123165539) do
+ActiveRecord::Schema.define(version: 20160124145613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20160123165539) do
   create_table "genres", force: :cascade do |t|
     t.string "name"
   end
+
+  create_table "opinions", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "user_id"
+    t.integer  "band_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "opinions", ["band_id"], name: "index_opinions_on_band_id", using: :btree
+  add_index "opinions", ["user_id"], name: "index_opinions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
