a9f5b5e1fea27679d2454159ee46839f48068b5b
Marek <NeveS07@azet.sk>
2016-01-25 21:05:28 +0100

    updated schema.rb after migration

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -47,6 +47,18 @@ ActiveRecord::Schema.define(version: 20160125190101) do
     t.datetime "cover_photo_updated_at"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text     "text"
+    t.boolean  "represents_band"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
   create_table "events", force: :cascade do |t|
     t.datetime "start_time"
     t.integer  "duration"
