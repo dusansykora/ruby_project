848b781a343a52c316db600244434de216d2fc4b
dusansykora <freewing.prod@gmail.com>
2016-01-25 17:43:49 +0100

    new Album model

---
+++ project/app/models/album.rb
@@ -0,0 +1,6 @@
+class Album < ActiveRecord::Base
+  has_attached_file :cover_photo,
+    :styles => { :medium => "300x300>", :thumb => "100x100#" },
+    :default_url => "/images/:style/missing_album_cover.png"
+  validates_attachment_content_type :cover_photo, :content_type => /\Aimage\/.*\Z/
+end
