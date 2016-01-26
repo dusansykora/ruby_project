387c54aee20d5b388117f41d61abf86b6072bc84
Marek <NeveS07@azet.sk>
2016-01-26 16:26:11 +0100

    refactoring of post controller

--- project/app/controllers/posts_controller.rb
+++ project/app/controllers/posts_controller.rb
@@ -1,20 +1,19 @@
 class PostsController < ApplicationController
+  before_action :fetch_current_band
+  before_action :fetch_current_post, only: [:show, :edit, :update, :destroy]
+  before_action :check_user_is_member_of_band, only: [:new, :create, :edit, :update, :destroy]
+  
   def index
-    @band = Band.find(params[:band_id])
   end
   
   def show
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:id])
   end
   
   def new
-    @band = Band.find(params[:band_id])
     @post = Post.new
   end
   
   def create
-    @band = Band.find(params[:band_id])
     @post = Post.new(post_params)
     @post.band_id = @band.id
     
@@ -26,14 +25,9 @@ class PostsController < ApplicationController
   end
   
   def edit
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:id])
   end
   
   def update
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:id])
-    
     if @post.update(post_params)
       redirect_to band_post_path(@band, @post)
     else
@@ -42,13 +36,28 @@ class PostsController < ApplicationController
   end
   
   def destroy
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:id])
+    @post.comments.each { |comment| comment.destroy }
     @post.destroy
     redirect_to band_posts_path(@band)
   end
   
   protected
+  
+  def fetch_current_band
+    @band = Band.find(params[:band_id])
+  end
+  
+  def fetch_current_post
+    @post = Post.find(params[:id])
+  end
+  
+  def check_user_is_member_of_band
+    @band = Band.find(params[:band_id])
+    if current_user.band_id != @band.id
+      flash[:alert] = "Access denied, you are not member of this band."
+      redirect_to band_posts_path(@band)
+    end
+  end
   
   def post_params
     params.require(:post).permit(:title, :text)
