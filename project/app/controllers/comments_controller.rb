964ef2ea0842b3ce75c10b5b0e98fa5a4b15d0cb
Marek <NeveS07@azet.sk>
2016-01-26 16:40:27 +0100

    refactoring of comment controller

--- project/app/controllers/comments_controller.rb
+++ project/app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
-class CommentsController < ApplicationController  
+class CommentsController < ApplicationController
+  before_action :fetch_current_band_and_post
+  before_action :fetch_current_comment, only: [:edit, :update, :destroy]
+  before_action :check_user_is_author, only: [:edit, :update, :destroy]
+  
   def new
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
   
   def create
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.post_id = @post.id
     @comment.user_id = current_user.id
@@ -20,16 +20,9 @@ class CommentsController < ApplicationController
   end
   
   def edit
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
   end
   
   def update
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    
     if @comment.update(comment_params)
       redirect_to band_post_path(@band, @post, anchor: 'comments')
     else
@@ -38,14 +31,31 @@ class CommentsController < ApplicationController
   end
   
   def destroy
-    @band = Band.find(params[:band_id])
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to band_post_path(@band, @post, anchor: 'comments')
   end
   
   protected
+  
+  def fetch_current_band_and_post
+    @band = Band.find(params[:band_id])
+    @post = Post.find(params[:post_id])
+  end
+  
+  def fetch_current_comment
+    @comment = Comment.find(params[:id])
+  end
+  
+  def check_user_is_author
+    @band = Band.find(params[:band_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    
+    if current_user.id != @comment.user_id
+      flash[:alert] = "Access denied, you are not author of this comment."
+      redirect_to band_post_path(@band, @post, anchor: 'comments')
+    end
+  end
   
   def comment_params
     params.require(:comment).permit(:text, :represents_band)
