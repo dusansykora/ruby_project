8448e828e90491724246e9a42e5e68dd6a8d6823
Marek <NeveS07@azet.sk>
2016-01-28 17:31:24 +0100

    changed info type in events

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126155916) do
+ActiveRecord::Schema.define(version: 20160128162705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20160126155916) do
     t.datetime "start_time"
     t.integer  "duration"
     t.string   "place"
-    t.string   "info"
+    t.text     "info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "band_id"
