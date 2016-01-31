7549939fe76278c656732b05268bc4b99e32eb9d
dusansykora <freewing.prod@gmail.com>
2016-01-31 12:58:21 +0100

    opinion index layout

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -3,11 +3,11 @@ class OpinionsController < ApplicationController
   before_action :fetch_current_opinion, only: [:edit, :update, :destroy]
   before_action :check_user_is_author, only: [:edit, :update, :destroy]
   before_action :check_user_has_not_written_opinion, only: [:new, :create]
-  
+
   def index
     @reaction = Reaction.new
   end
-  
+
   def new
     @opinion = Opinion.new
   end
@@ -16,7 +16,7 @@ class OpinionsController < ApplicationController
     @opinion = Opinion.new(opinion_params)
     @opinion.user_id = current_user.id
     @opinion.band_id = @band.id
-    
+
     if @opinion.save
        redirect_to band_opinions_path(@band)
     else
@@ -24,7 +24,7 @@ class OpinionsController < ApplicationController
     end
   end
 
-  def edit    
+  def edit
   end
 
   def update
@@ -40,17 +40,17 @@ class OpinionsController < ApplicationController
     @opinion.destroy
     redirect_to band_opinions_path(@band)
   end
-  
+
   protected
-  
+
   def fetch_current_band
     @band = Band.find(params[:band_id])
   end
-  
+
   def fetch_current_opinion
     @opinion = Opinion.find(params[:id])
   end
-  
+
   def check_user_is_author
     @opinion = Opinion.find(params[:id])
     if current_user.id != @opinion.user_id
@@ -58,7 +58,7 @@ class OpinionsController < ApplicationController
       redirect_to band_opinions_path(@opinion.band)
     end
   end
-  
+
   def check_user_has_not_written_opinion
     @band = Band.find(params[:band_id])
     if current_user.opinions.select{ |opinion| opinion.band_id == @band.id }.count > 0
